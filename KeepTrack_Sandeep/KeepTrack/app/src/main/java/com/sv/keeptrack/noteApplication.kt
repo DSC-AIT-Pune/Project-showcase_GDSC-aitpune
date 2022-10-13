391c70d54405774efac5ca4550c32881106693ff
Abhishek <abhiseknaulae@gmail.com>
2022-10-13 17:39:59 +0530

    Contributed by abhishek naula

--- KeepTrack_Sandeep/KeepTrack/app/src/main/java/com/sv/keeptrack/noteApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.sv.keeptrack
-
-import android.app.Application
-import com.sv.keeptrack.data.itemRoomDatabase
-
-class noteApplication:Application() {
-    val database:itemRoomDatabase by lazy{itemRoomDatabase.getDatabase(this)}
-}
